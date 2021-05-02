5578b4713eaccc1b4182fb7533b23d8c61132f05
Kari Johnson <kari@crownmediamarketing.com>
2021-05-01 21:27:53 -0600

    created db, installed packages, & created workouts

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # 17-Fitness-Tracker
+
+  ## Description
+  The purpose of this app is to allow users to track their workouts.  The user will be able to track the name, type, weight, sets, reps, and duration of exercise. If the exercise is a cardio exercise, they'll be able to track distance traveled.
+ 
+  ## Installation
+npm install 
+npm i express
+npm i morgan 
+npm i -D nodemon
+
+  ## Usage
+
+  ## License
+MIT
+  ## Screenshot
+
+  ## Resources
+  https://www.npmjs.com/
---
+++ models/workouts.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      name: {
+        type: String,
+        required: "Name",
+      },
+      type: {
+        type: String,
+        required: "What's the exercise?",
+      },
+      weight: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      duration: {
+        type: Number,
+        required: "How long was this session?",
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
