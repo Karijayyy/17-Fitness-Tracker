ddd37a3908556bd20b8a52bc13cab828af604c3f
Kari Johnson <kari@crownmediamarketing.com>
2021-05-03 12:20:32 -0600

    finished api route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,25 +5,58 @@ const db = require("../../models");
 
 //get last workout 
 router.get("/workouts", (req, res) => {
- 
+    .sort({ day: "asc" })
+    .then((workoutDB) => {
+      res.json(workoutDB);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+
   });
   
-// add workout 
+// adds workout 
   router.put("/workouts/:id", ({ params, body }, res) => {
     db.Workout.findByIdAndUpdate(
-   
+        params.id,
+        { $push: { exercises: body } },
+        { new: true }
+      )
+        .then((workoutDB) => {
+          res.json(workoutDB);
+        })
+        .catch((err) => {
+          res.json(err);
+        });
   });
   
 // this creates the workout 
   router.post("/workouts", ({ body }, res) => {
- 
+    db.Workout.create(body)
+    .then((workoutDB) => {
+      res.json(workoutDB);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
   });
   
   // this gets the workouts from the last sessions
   router.get("/workouts/range", (req, res) => {
     db.Workout.aggregate([
       {
-  
-  });
+        $addFields: {
+            totalDuration: { $sum: "$exercises.duration" },
+          },
+        },
+      ])
+        .then((workoutDB) => {
+          console.log(workoutDB);
+          res.json(workoutDB);
+        })
+        .catch((err) => {
+          res.json(err);
+        });
+    });
 
 module.exports = router;
