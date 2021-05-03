cf053cbc075938465079c0910340c400858c9298
Kari Johnson <kari@crownmediamarketing.com>
2021-05-03 12:04:34 -0600

    index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,29 @@
+// referenced week 14 mini project 
+
 const router = require("express").Router();
 const db = require("../../models");
 
-
+//get last workout 
+router.get("/workouts", (req, res) => {
+ 
+  });
+  
+// add workout 
+  router.put("/workouts/:id", ({ params, body }, res) => {
+    db.Workout.findByIdAndUpdate(
+   
+  });
+  
+// this creates the workout 
+  router.post("/workouts", ({ body }, res) => {
+ 
+  });
+  
+  // this gets the workouts from the last sessions
+  router.get("/workouts/range", (req, res) => {
+    db.Workout.aggregate([
+      {
+  
+  });
 
 module.exports = router;
